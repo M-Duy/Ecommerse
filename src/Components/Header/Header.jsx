026b4e45d3e12cc7d9a5bfd56d62dafb6e88e4fa
DuyNguyen <minhduy07062002@gmail.com>
2024-10-21 15:29:59 +0700

    Fixed Header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,6 +6,9 @@ import Logo from '@icons/images/Logo-retina.png';
 import reLoadIcon from '@icons/svgs/spinIcon.svg';
 import heartIcon from '@icons/svgs/heartIcon.svg';
 import cartIcon from '@icons/svgs/cartIcon.svg';
+import useScrollHandling from '@/hooks/useScrollHandling';
+import { useEffect, useState } from 'react';
+import classNames from 'classnames';
 
 function Header() {
     const {
@@ -14,10 +17,36 @@ function Header() {
         containerMenu,
         containerHeader,
         containerBox,
-        boxLogo
+        boxLogo,
+        fixedHeader,
+        topHeader
     } = styles;
+
+    const { scrollPosition } = useScrollHandling();
+    const [fixedPosition, setFixedPosition] = useState(false);
+
+    useEffect(() => {
+        //Cách 1:
+        // if (scrollPosition > 80) {
+        //     setFixedPosition(true);
+        // } else {
+        //     setFixedPosition(false);
+        // }
+
+        //Cách 2:
+        // setFixedPosition(scrollPosition > 80 ? true : false)
+
+        //Cách 3:
+        setFixedPosition(scrollPosition > 80);
+    }, [scrollPosition]);
+
+    console.log(scrollPosition);
     return (
-        <div className={container}>
+        <div
+            className={classNames(container, topHeader, {
+                [fixedHeader]: fixedPosition
+            })}
+        >
             <div className={containerHeader}>
                 <div className={containerBox}>
                     <div className={containerBoxIcon}>
